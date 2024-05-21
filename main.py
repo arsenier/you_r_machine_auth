b0c685ac4d16dbac38a33987ed77265922cd37c4
Arseniy <yarmolinskiyam@gmail.com>
2024-05-21 23:27:45 +0300

    basic auth and fictive token introspection

---
+++ main.py
@@ -0,0 +1,44 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+import uvicorn
+
+from core.config import settings
+from api_v1 import router as router_v1
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index!",
+    }
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}!"}
+
+
+@app.get("/calc/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "result": a + b,
+    }
+
+
+if __name__ == "__main__":
+    print("authenticate service online")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
